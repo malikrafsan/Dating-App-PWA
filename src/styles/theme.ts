d0e033f03b00561a1ed587d6a24a04ba297a4f92
Rifqi Naufal Abdjul <rifqi.abdjul23@gmail.com>
2023-02-17 11:08:24 +0700

    style: add default link style

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,20 +1,21 @@
 import { extendTheme, grid } from "@chakra-ui/react";
 import { ButtonStyles as Button } from "./components/buttonStyles";
 import { InputStyles as Input } from "./components/inputStyles";
+import { LinkStyles as Link } from "./components/linkStyles";
 
 const theme = extendTheme({
     colors: {
-        blue : {
-            "primary" : "#3E3684",
-            "secondary" : "#BFDBFF",
-            "light" : "#FBFBFF",
-            "dark" : "#312B67",
+        blue: {
+            "primary": "#3E3684",
+            "secondary": "#BFDBFF",
+            "light": "#FBFBFF",
+            "dark": "#312B67",
         },
-        pink : {
-            "primary" : "#FF60AF",
-            "secondary" : "#FFC1E3",
-            "light" : "#FFFBFD",
-            "dark" : "#EB3F94",
+        pink: {
+            "primary": "#FF60AF",
+            "secondary": "#FFC1E3",
+            "light": "#FFFBFD",
+            "dark": "#EB3F94",
         }
     },
     fonts: {
@@ -27,7 +28,7 @@ const theme = extendTheme({
                 bg: "white",
                 color: "blue.dark",
             },
-            "#app" : {
+            "#app": {
                 maxWidth: "30em",
                 margin: "0 auto",
                 bg: "blue.light",
@@ -39,6 +40,7 @@ const theme = extendTheme({
     components: {
         Button,
         Input,
+        Link,
     }
 
 });
