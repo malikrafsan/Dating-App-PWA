d911e9934ed527b9fe472b05838e770d0fcb5d0a
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2023-02-19 09:53:59 +0700

    feat: change app name and logo

--- vite.config.ts
+++ vite.config.ts
@@ -5,39 +5,39 @@ import { VitePWA } from 'vite-plugin-pwa'
 import replace from '@rollup/plugin-replace'
 
 const pwaOptions: Partial<VitePWAOptions> = {
-  mode: 'development',
-  base: '/',
-  includeAssets: ['favicon.svg'],
+  mode: "development",
+  base: "/",
+  includeAssets: ["favicon.svg"],
   manifest: {
-    name: 'PWA Router',
-    short_name: 'PWA Router',
-    theme_color: '#ffffff',
+    name: "CinLok",
+    short_name: "CinLok",
+    theme_color: "#ffffff",
     icons: [
       {
-        src: 'pwa-192x192.png', // <== don't add slash, for testing
-        sizes: '192x192',
-        type: 'image/png',
+        src: "pwa-192x192.png", // <== don't add slash, for testing
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        src: '/pwa-512x512.png', // <== don't remove slash, for testing
-        sizes: '512x512',
-        type: 'image/png',
+        src: "/pwa-512x512.png", // <== don't remove slash, for testing
+        sizes: "512x512",
+        type: "image/png",
       },
       {
-        src: 'pwa-512x512.png', // <== don't add slash, for testing
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
+        src: "pwa-512x512.png", // <== don't add slash, for testing
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
       },
     ],
   },
   devOptions: {
-    enabled: process.env.SW_DEV === 'true',
+    enabled: process.env.SW_DEV === "true",
     /* when using generateSW the PWA plugin will switch to classic */
-    type: 'module',
-    navigateFallback: 'index.html',
+    type: "module",
+    navigateFallback: "index.html",
   },
-}
+};
 
 const replaceOptions = { __DATE__: new Date().toISOString() }
 const claims = process.env.CLAIMS === 'true'
